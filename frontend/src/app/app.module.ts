88d67a166acb38457b7c6276d4d576a9a68beb1c
Glaucio <glaucioripol@gmail.com>
2020-04-26 19:05:52 -0300

    finalizado componente footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// importando componentes de terceiro
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './component/template/header/header.component';
-
-// importando componentes de terceiro
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './component/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
